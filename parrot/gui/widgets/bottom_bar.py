67114e45383a0b7979d39a0af00db1ad9b48cb95
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-09-11 22:47:25 +0100

    Sidebar's bottom bar's buttons now all have signals

--- parrot/gui/widgets/bottom_bar.py
+++ parrot/gui/widgets/bottom_bar.py
@@ -1,19 +1,27 @@
+from PySide6.QtCore import Signal
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QPushButton
 
 
 class BottomBar(QWidget):
+    settings_clicked = Signal()
+    theme_clicked = Signal()
+    search_clicked = Signal()
+
     def __init__(self):
         super().__init__()
 
         self.layout = QHBoxLayout()
 
         settings_button = QPushButton("Settings")
+        settings_button.clicked.connect(self.settings_clicked.emit)
         self.layout.addWidget(settings_button)
 
         theme_button = QPushButton("Light/Dark Mode")
+        theme_button.clicked.connect(self.theme_clicked.emit)
         self.layout.addWidget(theme_button)
 
         search_button = QPushButton("Search")
+        search_button.clicked.connect(self.search_clicked.emit)
         self.layout.addWidget(search_button)
 
         self.setLayout(self.layout)
