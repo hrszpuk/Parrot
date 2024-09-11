369e7021e756b9e7060c835595d3652e421d2b02
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-09-11 20:21:17 +0100

    Refactored chat window using top bar widget

--- parrot/gui/widgets/chat_window.py
+++ parrot/gui/widgets/chat_window.py
@@ -1,44 +1,36 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout
 
 from parrot.gui.widgets.chat_box import ChatBox
 from parrot.gui.widgets.chat_message import ChatMessage
 from parrot.gui.widgets.chat_message_list import ChatMessageList
+from parrot.gui.widgets.top_bar import TopBar
 
 
-# The rest of your code remains unchanged
 class ChatWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        layout = QVBoxLayout()
+        layout = QHBoxLayout()
 
-        self.top_bar = ChatWindow.create_top_bar(chat_name="Conversation Name")
-        layout.addWidget(self.top_bar)
+        chat_layout = QVBoxLayout()
+        chat_widget = QWidget()
+        chat_widget.setLayout(chat_layout)
+
+        chat_top_bar = TopBar(title="Chat Name")
+        chat_layout.addWidget(chat_top_bar)
 
         self.message_list = ChatMessageList()
-        layout.addWidget(self.message_list)
+        chat_layout.addWidget(self.message_list)
 
-        self.chat_box = ChatBox()
-        self.chat_box.message_sent.connect(self.add_message)
+        chat_box = ChatBox()
+        chat_box.message_sent.connect(self.add_message)
 
-        layout.addWidget(self.chat_box)
+        chat_layout.addWidget(chat_box)
+
+        layout.addWidget(chat_widget)
 
         self.setLayout(layout)
 
     def add_message(self, text, is_user=True, loading=False):
         message_widget = ChatMessage(text, is_user, loading)
         self.message_list.add_message(message_widget)
-
-    @staticmethod
-    def create_top_bar(chat_name):
-        """Creates a top bar widget with a title label."""
-        top_bar = QWidget()
-        layout = QVBoxLayout()
-
-        title_label = QLabel(chat_name)
-        title_label.setStyleSheet("font-size: 18px; font-weight: bold;")
-        layout.addWidget(title_label)
-
-        layout.addStretch()
-        top_bar.setLayout(layout)
-        return top_bar
