ff87b04a4192a0b34cfba59a98c5cbc86f8366ac
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-09-11 20:57:20 +0100

    Sidebar is complete (apart from a few components)

--- parrot/gui/widgets/sidebar.py
+++ parrot/gui/widgets/sidebar.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout
 
 from parrot.gui.widgets.bottom_bar import BottomBar
+from parrot.gui.widgets.chat_list import ChatList
+from parrot.gui.widgets.model_list import ModelList
 from parrot.gui.widgets.top_bar import TopBar
 
 
@@ -13,6 +15,12 @@ class Sidebar(QWidget):
 
         top = TopBar(title="Parrot")
         self.layout.addWidget(top)
+
+        model_list = (ModelList())
+        self.layout.addWidget(model_list)
+
+        chat_list = ChatList()
+        self.layout.addWidget(chat_list)
 
         bottom = BottomBar()
         self.layout.addWidget(bottom)
