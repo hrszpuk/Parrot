84bdc6434040a508398933d8e2648d1f61385f7b
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-09-12 10:00:33 +0100

    Updated core imports

--- parrot/core/__init__.py
+++ parrot/core/__init__.py
@@ -1,5 +1,5 @@
 """
 This is the core module for parrot. This module contains all the "business" logic used in the application.
 """
-from . import manager
-from . import interface
+from manager import ModelManager
+from interface import ModelInterface
